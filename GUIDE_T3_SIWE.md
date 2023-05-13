e9cfb1006008938dfe253ca1556adc086a3bd7a3
ben <junhuangho27@gmail.com>
2023-05-13 17:00:10 +0800

    first commit

---
+++ GUIDE_T3_SIWE.md
@@ -0,0 +1,14 @@
+[for next-auth: "^4.21.0" - unsure for future versions]
+
+1. yarn create t3-app
+2. maintain next to be "^13.2.6" [not strictly related to t3+SIWE]
+3. install @rainbow-me/rainbowkit >= 1.0.0
+4. install @rainbow-me/rainbowkit-siwe-next-auth >= 0.2.0
+5. install "ethers": "^5.6.8",
+6. install "siwe": "^2.1.4",
+7. install "viem": "~0.3.19", - TEST
+8. install "wagmi": "^1.0.1"
+9. rmb add NEXTAUTH_SECRET in .env
+10. edit `server/auth.ts` according to https://github.com/codingwithmanny/t3-app-siwe/tree/fix/next-auth-4.21.1
+11. edit `pages/api/auth/[...nextauth].ts` according to https://github.com/codingwithmanny/t3-app-siwe/tree/fix/next-auth-4.21.1
+12. edit `_app.tsx` and `index.tsx` as usual
---
+++ src/pages/_app.tsx
@@ -0,0 +1,80 @@
+import { type AppType } from "next/app";
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+
+import { api } from "~/utils/api";
+
+import "~/styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+
+// SIWE Integration
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+  connectorsForWallets,
+} from "@rainbow-me/rainbowkit";
+import { metaMaskWallet } from "@rainbow-me/rainbowkit/wallets";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, goerli } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+import {
+  RainbowKitSiweNextAuthProvider,
+  GetSiweMessageOptions,
+} from "@rainbow-me/rainbowkit-siwe-next-auth";
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [goerli] : []),
+  ],
+  [publicProvider()]
+);
+/**
+ * Configure client with providers and allow for auto wallet connection
+ */
+const connectors = connectorsForWallets([
+  {
+    groupName: "Other",
+    wallets: [metaMaskWallet({ chains })],
+  },
+]);
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+});
+
+const getSiweMessageOptions: GetSiweMessageOptions = () => ({
+  statement: "Sign in to the RainbowKit + SIWE example app",
+});
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <WagmiConfig config={wagmiConfig}>
+        <RainbowKitSiweNextAuthProvider
+          getSiweMessageOptions={getSiweMessageOptions}
+        >
+          <RainbowKitProvider
+            appInfo={{
+              appName: "Rainbowkit Demo",
+            }}
+            chains={chains}
+          >
+            <Component {...pageProps} />
+          </RainbowKitProvider>
+        </RainbowKitSiweNextAuthProvider>
+      </WagmiConfig>
+    </SessionProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
